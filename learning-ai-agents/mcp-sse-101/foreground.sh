a1b644400eae565b6b1dec4533fa1cf7eed7cb11
Pranay <pranay.shah@gmail.com>
2025-03-29 10:58:48 -0700

    Updated docs - and http link of agent

--- learning-ai-agents/mcp-sse-101/foreground.sh
+++ learning-ai-agents/mcp-sse-101/foreground.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-set +x  # turns OFF command echoing
 
 echo "Waiting for init-script to finish..."
 while [ ! -f /tmp/background0 ]; do sleep 1; done
